63fca3dbe4df7df3cc2e9ca29c388001eb1d6612
soohyun-bae <qosuh58@gmail.com>
2024-11-08 14:30:56 +0900

    if문으로 만들깅

--- script.js
+++ script.js
@@ -1,84 +1,44 @@
-const display = document.querySelector('span')
-let firstOperand = null
-let currentText = null
-let operator = ''
+const buttons = document.querySelectorAll('.button')
+const display = document.querySelector('.display')
 
-function firstProcess(clickedButton) {
-  const targetText = clickedButton.target
 
-  if (targetText.classList.contains('number')) {
-    displayNumber(targetText.textContent)
-    display.textContent = currentText
-  } else if (clickedButton.target.classList.contains('dot')) {
-    displayDot()
-  } else if (targetText.textContent === 'C') {
-    resetDisplay()
-  } else if (targetText.classList.contains('operator')) {
-    saveOperator(targetText.textContent)
-  } else if (targetText.textContent === '=') {
-    displayResult()
-  }
-}
+buttons.forEach((eachButton) => {
+  eachButton.addEventListener('click', () => { // 각 버튼에 클릭 이벤트 추가
+    const text = eachButton.textContent
+    let displayText = display.textContent
 
-function displayNumber(text) {
-  if (currentText === null) {
-    currentText = text
-  } else {
-    currentText += text
-  }
-}
+    let firstOperand = '';
+    let operator = '';
 
-function displayDot() {
-  if (!currentText.includes('.')) {
-    currentText += '.'
-    display.textContent = currentText
-  }
-}
+    if (text === 'C') {
+      displayText = '0'
+    }
 
-function resetDisplay() {
-  display.textContent = "0"
-  currentText = null
-  firstOperand = null
-}
+    if (displayText === '0') { // 0일때
+      if (eachButton.classList.contains('number')) {
+        displayText = text
+      } else if (eachButton.classList.contains('dot')) {
+        displayText = '0.'
+      }
+    } else { // 0 아닐 떼
+      if (eachButton.classList.contains('number')) {
+        displayText += text
+      } else if (eachButton.classList.contains('dot')) {
+        displayText += displayText.includes('.') ? '' : '.'
+      } else if (eachButton.classList.contains('operator')) {
+        if (firstOperand === '') {
+          operator = text
+          firstOperand = displayText
+          displayText = '0'
+          console.log(`First Operand: ${firstOperand}, Operator: ${operator}`)
+        }
+      }
+    }
+    const calculate = (num1, num2, num3) => {
+      
+    }
 
-function saveOperator(text) {
-  if (firstOperand !== null && operator !== '') {
-    const secondOperand = parseFloat(display.textContent)
-    const result = calculate(firstOperand, secondOperand, operator)
-    firstOperand = result
-    display.textContent = result
-  } else {
-    firstOperand = parseFloat(display.textContent)
-    //display.textContent = "0"
-  }
-  operator = text
-  currentText = null
-  console.log(`First Operand : ${firstOperand}, Operator: ${operator}`)
-}
-
-function calculate(num1, num2, operator) {
-  switch (operator) {
-    case '/':
-      return num1 / num2;
-    case '*':
-      return num1 * num2;
-    case '-':
-      return num1 - num2;
-    case '+':
-      return num1 + num2;
-  }
-}
-
-function displayResult() {
-  if (firstOperand !== null && currentText !== '') {
-    const secondOperand = parseFloat(currentText)
-    const result = calculate(firstOperand, secondOperand, operator)
-    display.textContent = result
-    firstOperand = result
-    operator = ''
-  }
-}
-
-document.querySelectorAll('.button').forEach((eachButton) => {
-  eachButton.addEventListener('click', firstProcess)
+    display.textContent = displayText
+  })
 })
+
